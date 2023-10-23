3303bb2df155fd0796fd62cf093d86d43c144291
Marina Rull Valdivieso <marinarullvaldivieso@Marinas-MacBook-Pro-26.local>
2023-10-23 18:59:25 +0200

    first draft

---
+++ src/components/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Menu() {
+    return (
+      <div className="menu">
+        <ul>
+          <li><a href="#">Deportes</a></li>
+          <li><a href="#">Mujer</a></li>
+          <li><a href="#">Hombre</a></li>
+          <li><a href="#">Infantil</a></li>
+          <li><a href="#">Equipamiento</a></li>
+          <li><a href="#">Nutrición y salud</a></li>
+          <li><a href="#">Descuentos</a></li>
+        </ul>
+      </div>
+    );
+  }
+
+  export default Menu;
