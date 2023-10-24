be844c290f5881ceaa81af98f5a8fb2979481bde
Marina Rull Valdivieso <marinarullvaldivieso@Marinas-MacBook-Pro-26.local>
2023-10-24 11:05:38 +0200

    changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import './styles.css';
+import Logo from '../assets/logo.png'; 
 
 function Menu() {
-    return (
-      <div className="menu">
-        <ul>
-          <li><a href="#">Deportes</a></li>
-          <li><a href="#">Mujer</a></li>
-          <li><a href="#">Hombre</a></li>
-          <li><a href="#">Infantil</a></li>
-          <li><a href="#">Equipamiento</a></li>
-          <li><a href="#">Nutrición y salud</a></li>
-          <li><a href="#">Descuentos</a></li>
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className="menu">
+      <ul>
+      <img class="logo" src={Logo} alt="Decathlon logo" />
+        <li><a href="#">Deportes</a></li>
+        <li><a href="#">Mujer</a></li>
+        <li><a href="#">Hombre</a></li>
+        <li><a href="#">Infantil</a></li>
+        <li><a href="#">Equipamiento</a></li>
+        <li><a href="#">Nutrición y salud</a></li>
+        <li><a href="#">Descuentos</a></li>
+      </ul>
+    </div>
+  );
+  } 
 
   export default Menu;
